b16e8941a0193380aacee3adfbfe8e4f4ae9c82d
Ggmerda <eduardinhoggsouza@gmail.com>
2024-04-11 14:34:22 -0300

    correçao buzz

--- arq.ino
+++ arq.ino
@@ -1,9 +1,9 @@
 // Definição dos pinos conectados aos componentes e as variáveis
 int intensidade;
 int pinLDR = A0;  // Pino analógico conectado ao sensor LDR
-int pinVER = 13;  // Pino digital conectado ao LED verde
+int pinVER = 13;  // Pino digital conectado ao LED vermelho
 int pinAM = 12;   // Pino digital conectado ao LED amarelo
-int pinVED = 11;  // Pino digital conectado ao LED vermelho
+int pinVED = 11;  // Pino digital conectado ao LED verde
 int pinBUZ = 7;   // Pino digital conectado ao buzzer
 
 // Configuração inicial do Arduino
@@ -37,37 +37,43 @@ void loop() {
   // Verifica o valor lido pelo sensor LDR e controla os dispositivos conectados aos pinos de saída
   if (valorLDR >= 900) {
     // Se a luz for muito forte (valorLDR maior ou igual a 900)
-    digitalWrite(pinVER, LOW);   // Liga o LED verde
+    digitalWrite(pinVER, LOW);   // Liga o LED vermelho
     digitalWrite(pinAM, HIGH);   // Desliga o LED amarelo
-    digitalWrite(pinVED, HIGH);  // Desliga o LED vermelho
-    tone(pinBUZ, 1000);  // Ativa o buzzer com frequência de 1000 Hz
-    delay(3000); // Aguarda 3 segundos
-    noTone(pinBUZ);  // Desliga o buzzer
+    digitalWrite(pinVED, HIGH);  // Desliga o LED verde
+    digitalWrite(pinBUZ, HIGH);  // DesLiga o buzzer
+
+
   } else if (valorLDR <= 899 && valorLDR >= 700) {
     // Se a luz for moderadamente forte (valorLDR entre 700 e 899)
-    digitalWrite(pinVER, HIGH);  // Desliga o LED verde
-    digitalWrite(pinVED, HIGH);  // Desliga o LED vermelho
+    digitalWrite(pinVER, HIGH);  // Desliga o LED vermelho
+    digitalWrite(pinVED, HIGH);  // Desliga o LED verde
     digitalWrite(pinAM, LOW);    // Liga o LED amarelo
-    digitalWrite(pinBUZ, HIGH);  // Liga o buzzer
+    tone(pinBUZ, 1000);  // Ativa o buzzer com frequência de 1000 Hz
+    delay(3000);
+    noTone(pinBUZ);// desliga som
+
   } else if (valorLDR <= 500 && valorLDR >= 401) {
     // Se a luz for moderadamente fraca (valorLDR entre 401 e 500)
     digitalWrite(pinAM, LOW);    // Liga o LED amarelo
-    digitalWrite(pinVED, HIGH);  // Desliga o LED vermelho
-    digitalWrite(pinVER, HIGH);  // Desliga o LED verde
-    digitalWrite(pinBUZ, HIGH);  // Liga o buzzer
+    digitalWrite(pinVED, HIGH);  // Desliga o LED verde
+    digitalWrite(pinVER, HIGH);  // Desliga o LED vermelho
+    tone(pinBUZ, 1000);  // Ativa o buzzer com frequência de 1000 Hz
+    delay(3000);
+    noTone(pinBUZ);// desliga som
+
   } else if (valorLDR <= 400) {
     // Se a luz for muito fraca (valorLDR menor ou igual a 400)
-    digitalWrite(pinVER, LOW);   // Liga o LED verde
+    digitalWrite(pinVER, LOW);   // Liga o LED vermelho
     digitalWrite(pinAM, HIGH);   // Desliga o LED amarelo
-    digitalWrite(pinVED, HIGH);  // Desliga o LED vermelho
-    tone(pinBUZ, 1000);  // Ativa o buzzer com frequência de 1000 Hz
-    delay(3000);                 // Aguarda 3 segundos
-    noTone(pinBUZ);  // Desliga o buzzer
+    digitalWrite(pinVED, HIGH);  // Desliga o LED verde
+    digitalWrite(pinBUZ, HIGH);  // DLiga o buzzer
+
+
   } else {
     // Se nenhuma das condições acima for atendida
-    digitalWrite(pinVER, HIGH);  // Desliga o LED verde
+    digitalWrite(pinVER, HIGH);  // Desliga o LED vermelho
     digitalWrite(pinAM, HIGH);   // Desliga o LED amarelo
-    digitalWrite(pinVED, LOW);   // Liga o LED vermelho
-    digitalWrite(pinBUZ, HIGH);  // Liga o buzzer 
+    digitalWrite(pinVED, LOW);   // Liga o LED verde
+    digitalWrite(pinBUZ, HIGH);  // DLiga o buzzer 
   }
 }

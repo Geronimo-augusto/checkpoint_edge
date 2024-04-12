b845bd15d0a435a7b5ef191043e717f4ebfbb120
Geronimo-augusto <126916675+Geronimo-augusto@users.noreply.github.com>
2024-04-12 07:34:43 -0300

    Update arq.ino

--- arq.ino
+++ arq.ino
@@ -1,9 +1,9 @@
-// Definição dos pinos conectados aos componentes e as variáveis
+// Definição dos pinos conectados aos componentes e as varaveis
 int intensidade;
 int pinLDR = A0;  // Pino analógico conectado ao sensor LDR
-int pinVER = 13;  // Pino digital conectado ao LED vermelho
+int pinVER = 13;  // Pino digital conectado ao LED verde
 int pinAM = 12;   // Pino digital conectado ao LED amarelo
-int pinVED = 11;  // Pino digital conectado ao LED verde
+int pinVED = 11;  // Pino digital conectado ao LED vermelho
 int pinBUZ = 7;   // Pino digital conectado ao buzzer
 
 // Configuração inicial do Arduino
@@ -24,56 +24,65 @@ void loop() {
   // Lê o valor analógico do sensor LDR e armazena na variável "valorLDR"
   int valorLDR = analogRead(pinLDR);
   
-  // Mapeia o valor lido do sensor LDR para um intervalo de 0 a 100 para representar a intensidade da luz
-  intensidade = map(valorLDR, 182, 969, 0, 100);
+  intensidade = map(valorLDR,182, 969, 0, 100);
   
-  // Imprime o valor do sensor LDR e a intensidade da luz no monitor serial
+  // Imprime o valor do sensor LDR no monitor serial
   Serial.println(valorLDR);
   Serial.print(intensidade); Serial.println("%");                 
   
-  // Aguarda 550 milissegundos antes de continuar a execução do código
-  delay(550);
   
   // Verifica o valor lido pelo sensor LDR e controla os dispositivos conectados aos pinos de saída
   if (valorLDR >= 900) {
     // Se a luz for muito forte (valorLDR maior ou igual a 900)
-    digitalWrite(pinVER, LOW);   // Liga o LED vermelho
-    digitalWrite(pinAM, HIGH);   // Desliga o LED amarelo
-    digitalWrite(pinVED, HIGH);  // Desliga o LED verde
-    digitalWrite(pinBUZ, HIGH);  // DesLiga o buzzer
+    digitalWrite(pinVER, HIGH);   // liga o LED vermelho
+    digitalWrite(pinAM, LOW);   // desLiga o LED amarelo
+    digitalWrite(pinVED, LOW);  // desLiga o LED verde
 
-
+    // Ativa o buzzer para indicação de perigo
+    tone(pinBUZ, 200);
+    delay(3000); // Ativa o buzzer por 3 segundos
+    noTone(pinBUZ); // Desliga o buzzer
+    
+    
   } else if (valorLDR <= 899 && valorLDR >= 700) {
     // Se a luz for moderadamente forte (valorLDR entre 700 e 899)
-    digitalWrite(pinVER, HIGH);  // Desliga o LED vermelho
-    digitalWrite(pinVED, HIGH);  // Desliga o LED verde
-    digitalWrite(pinAM, LOW);    // Liga o LED amarelo
-    tone(pinBUZ, 1000);  // Ativa o buzzer com frequência de 1000 Hz
-    delay(3000);
-    noTone(pinBUZ);// desliga som
-
+    digitalWrite(pinVER, LOW);  // desLiga o LED vermelho
+    digitalWrite(pinAM, HIGH);    // liga o LED amarelo
+    digitalWrite(pinVED, LOW);  // desLiga o LED verde
+    
+    
+    // Ativa o buzzer para indicação de aviso
+    tone(pinBUZ, 400);
+    delay(3000); // Ativa o buzzer por 3 segundos
+    noTone(pinBUZ); // Desliga o buzzer
+    
   } else if (valorLDR <= 500 && valorLDR >= 401) {
     // Se a luz for moderadamente fraca (valorLDR entre 401 e 500)
-    digitalWrite(pinAM, LOW);    // Liga o LED amarelo
-    digitalWrite(pinVED, HIGH);  // Desliga o LED verde
-    digitalWrite(pinVER, HIGH);  // Desliga o LED vermelho
-    tone(pinBUZ, 1000);  // Ativa o buzzer com frequência de 1000 Hz
-    delay(3000);
-    noTone(pinBUZ);// desliga som
-
+    digitalWrite(pinVER, LOW);  // desLiga o LED vermelho
+    digitalWrite(pinAM, HIGH);    // liga o LED amarelo
+    digitalWrite(pinVED, LOW);  // desLiga o LED verde
+    
+    // Ativa o buzzer para indicação de aviso
+    tone(pinBUZ, 400);
+    delay(3000); // Ativa o buzzer por 3 segundos
+    noTone(pinBUZ); // Desliga o buzzer
+    
   } else if (valorLDR <= 400) {
     // Se a luz for muito fraca (valorLDR menor ou igual a 400)
-    digitalWrite(pinVER, LOW);   // Liga o LED vermelho
-    digitalWrite(pinAM, HIGH);   // Desliga o LED amarelo
-    digitalWrite(pinVED, HIGH);  // Desliga o LED verde
-    digitalWrite(pinBUZ, HIGH);  // DLiga o buzzer
-
-
+    digitalWrite(pinVER, HIGH);   // liga o LED vermelho
+    digitalWrite(pinAM, LOW);   // desLiga o LED amarelo
+    digitalWrite(pinVED, LOW);  // desLiga o LED verde
+	
+    // Ativa o buzzer para indicação de perigo
+    tone(7, 200);
+    delay(3000); // Ativa o buzzer por 3 segundos
+    noTone(7); // Desliga o buzzer
+    
   } else {
     // Se nenhuma das condições acima for atendida
-    digitalWrite(pinVER, HIGH);  // Desliga o LED vermelho
-    digitalWrite(pinAM, HIGH);   // Desliga o LED amarelo
-    digitalWrite(pinVED, LOW);   // Liga o LED verde
-    digitalWrite(pinBUZ, HIGH);  // DLiga o buzzer 
+    digitalWrite(pinVER, LOW);  // desLiga o LED vermelho
+    digitalWrite(pinAM, LOW);   // desLiga o LED amarelo
+    digitalWrite(pinVED, HIGH);   // liga o LED verde
   }
+  delay(1000);// Atraso para estabilidade
 }
